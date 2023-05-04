b0d8e9cf20817a113e6e16c2119d55c6d149de6d
Gnanasekar308 <124139265+Gnanasekar308@users.noreply.github.com>
2023-05-04 08:57:23 +0530

    Add files via upload

---
+++ gym-web/main.js
@@ -0,0 +1,31 @@
+$('.gym-workouts').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+var owl = $('.brands');
+owl.owlCarousel({
+    items:5,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+// $('.play').on('click',function(){
+//     owl.trigger('play.owl.autoplay',[1000])
+// })
+// $('.stop').on('click',function(){
+//     owl.trigger('stop.owl.autoplay')
+// })
